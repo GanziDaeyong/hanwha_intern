cac7d3fa1f334f77476214cf7577bb84ae866551
Jeong Daeyong <ganzidaeyong@wafflestudio.com>
2022-02-07 17:35:45 +0900

    fuction/workingon_token send

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,8 @@
+const human_abi = require("../import/human_abi");
+
 // Web3 & Ethereum dependency
 var web3 = new Web3(
   "https://ropsten.infura.io/v3/3c52917848e945229c0d33d632b10490"
 );
 var Buffer = ethereumjs.Buffer.Buffer;
+var abi = human_abi;
--- js/test.js
+++ js/test.js
@@ -5,122 +5,150 @@ $("#reset").click(function () {
 });
 
 $("#test").click(function () {
-  // console.log(web3.eth.accounts.wallet.load("1"));
-  // chrome.storage.sync.get(null, function (obj) {
-  //   console.log(obj.txbuf);
-  // });
-  CheckTxBuffer();
-  // t();
+  console.log("gogo");
+  // f_deploy_sc();
+  // GetBytecode("sooong", "sgt", 3000000000000);
+  // tokbal();
+  sendTok();
+  // _SendMsg("_0601");
 });
 
-$("#_0201_button_log").click(function () {
-  GetHistory();
+$("#_0601_button_create").click(function () {
+  _SendMsg("_0602");
 });
 
-$("#_0501_button_update").click(function () {
-  CheckTxBuffer();
-  GetHistory();
+async function GetBytecode(name, sym, totalsupply) {
+  const l = "http://localhost:8080/api/v1/createtoken";
+
+  const response1 = await fetch(
+    // "http://115.85.181.243:8080/api/v1/createtoken"
+    l,
+    {
+      method: "POST",
+      body: JSON.stringify({ name: name, sym: sym, totalsupply: totalsupply }),
+    }
+  );
+  const result1 = await response1.json();
+  const bytecode = result1["bytecode"];
+  console.log(result1);
+  // let bytecode = ; // test
+  const t = f_deploy_sc(bytecode);
+}
+
+async function f_deploy_sc(bytecode) {
+  // TODO: Write
+  console.log("Begin deployment");
+
+  const curr = await _GetCurr();
+  const fromAddress = curr["address"];
+  const pw = await _GetWalletPW();
+  const walletobj = web3.eth.accounts.wallet.load(pw);
+  let pk = walletobj[fromAddress].privateKey;
+  console.log(pk);
+  pk = pk.substring(2);
+  console.log(pk);
+
+  const privateKey = Buffer.from(pk, "hex");
+  const data = "0x" + bytecode;
+
+  console.log("Begin deployment2");
+
+  // let gasPrice = await _GetAvgGasPrice();
+  // gasPrice = web3.utils.fromWei(gasPrice, "ether"); // toether
+  // let block = await web3.eth.getBlock("latest");
+  // let gasLimit = block.gasLimit / block.transactions.length;
+  // gasLimit = parseInt(gasLimit) + 100000;
+  // console.log(gasLimit);
+  console.log("Begin deployment3");
+
+  web3.eth.getTransactionCount(fromAddress, (err, txCount) => {
+    const txObject = {
+      nonce: web3.utils.toHex(txCount),
+      gasLimit: web3.utils.toHex(2000000), // Raise the gas limit to a much higher amount
+      gasPrice: web3.utils.toHex(web3.utils.toWei("80", "gwei")), // 얘를 가져와서 쓰기
+      // gasLimit: web3.utils.toHex(gasLimit), // Raise the gas limit to a much higher amount
+      // gasPrice: web3.utils.toHex(web3.utils.toWei(gasPrice, "ether")),
+      data: data,
+    };
+
+    const tx = new ethereumjs.Tx(txObject, { chain: "ropsten" });
+    tx.sign(privateKey);
+    const serializedTx = tx.serialize();
+    const raw = "0x" + serializedTx.toString("hex");
+    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+      console.log(err);
+      console.log(txHash);
+    });
+  });
+}
+
+function sendTok() {
+  // let abi = require("human-standard-token-abi");
+  // console.log(abi);
+
+  console.log(abi);
+}
+
+// send token
+/*
+
+var count = web3.eth.getTransactionCount("0x26...");
+var abiArray = JSON.parse(fs.readFileSync('mycoin.json', 'utf-8'));
+var contractAddress = "0x8...";
+var contract = web3.eth.contract(abiArray).at(contractAddress);
+var rawTransaction = {
+    "from": "0x26...",
+    "nonce": web3.toHex(count),
+    "gasPrice": "0x04e3b29200",
+    "gasLimit": "0x7458",
+    "to": contractAddress,
+    "value": "0x0",
+    "data": contract.transfer.getData("0xCb...", 10, {from: "0x26..."}),
+    "chainId": 0x03
+};
+
+var privKey = new Buffer('fc3...', 'hex');
+var tx = new Tx(rawTransaction);
+
+tx.sign(privKey);
+var serializedTx = tx.serialize();
+
+web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
+    if (!err)
+        console.log(hash);
+    else
+        console.log(err);
 });
+*/
 
-// async function GetHistory() {
-//   // let PENDING_NUM = 0; // PENDING LOGIC
-//   let obj = await _GetAll();
-//   console.log(obj);
-//   const currIdx = obj["currAcc"];
-//   let history = obj["accList"][currIdx]["history"];
-//   const txBuf = obj["txbuf"];
+/* get token balance */
+async function tokbal() {
+  let dec = 1e-18;
+  let tokenAddress = "0x5bD82754D73e98139d0356F3607b377402E747a7";
+  // let walletAddress = "0xa7c1b07c893ca1934bb4f5730a3008503e804f5d";
+  let walletAddress = "0x5CCE38322F190EAB8Abc7Ceb23E816Cf7d3b48DC";
+  // The minimum ABI to get ERC20 Token balance
+  let minABI = [
+    // balanceOf
+    {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+    },
+    // decimals
+    {
+      constant: true,
+      inputs: [],
+      name: "decimals",
+      outputs: [{ name: "", type: "uint8" }],
+      type: "function",
+    },
+  ];
 
-//   let pendingMsg = "";
-//   for (let i = 0; i < txBuf.length; i++) {
-//     if (
-//       txBuf[i].txStatus == -1 &&
-//       txBuf[i].from == obj["accList"][currIdx]["address"]
-//     ) {
-//       pendingMsg += "[Pending]\n" + txBuf[i].txHash + "\n";
-//     }
-//   }
-
-//   let msg = "";
-//   for (let hist of history) {
-//     let eachHis =
-//       "[" +
-//       hist.txStatus +
-//       "]" +
-//       "\ntime - " +
-//       hist.time +
-//       "\ntype - " +
-//       hist.txType +
-//       "\ncurrency - " +
-//       hist.currencyType +
-//       "\nto - " +
-//       hist.to +
-//       "\namount - " +
-//       hist.amount +
-//       "\n";
-//     eachHis += "-------------------------\n";
-//     msg += eachHis;
-//   }
-//   msg = pendingMsg + msg;
-//   console.log(msg);
-//   _SendMsg("_0501", msg);
-// }
-
-// function _GetIdx(accList, address) {
-//   let res;
-//   for (let i = 0; i < accList.length; i++) {
-//     if (accList[i]["address"] == address) {
-//       res = i;
-//       break;
-//     }
-//   }
-//   console.log(res);
-//   return res;
-// }
-
-// async function CheckTxBuffer() {
-//   let obj = await _GetAll();
-//   console.log(obj);
-//   const txBuf = obj["txbuf"];
-//   const currIdx = obj["currAcc"];
-//   let changed = false;
-
-//   let PENDING_NUM = 0;
-
-//   for (let i = 0; i < txBuf.length; i++) {
-//     const fromAddress = txBuf[i]["from"];
-//     const eachIdx = _GetIdx(obj["accList"], fromAddress);
-//     console.log(txBuf[i].txHash);
-//     if (txBuf[i].txStatus == -1) {
-//       PENDING_NUM += 1;
-//       console.log("DETECTED");
-//       const status = await _CheckTxStatus(txBuf[i].txHash);
-//       console.log(status);
-//       if (status == 1) {
-//         console.log("DETECTEDA");
-//         obj["txbuf"][i].txStatus = "success";
-//         obj["accList"][eachIdx]["history"].push(obj["txbuf"][i]); //
-//         changed = true;
-//       } else if (status == 0) {
-//         txBuf[i].txStatus = "fail";
-//         console.log("DETECTEDB");
-
-//         obj["accList"][eachIdx]["history"].push(obj["txbuf"][i]);
-//         changed = true;
-//       }
-//     }
-//   }
-//   if (changed) {
-//     chrome.storage.sync.set(obj, function () {
-//       console.log("Saved");
-//       console.log(obj);
-//     });
-//   }
-//   if (txBuf.length != 0 && PENDING_NUM === 0) {
-//     obj["txbuf"] = [];
-//     chrome.storage.sync.set(obj, function () {
-//       console.log("TxBuffer Emptied");
-//       console.log(obj);
-//     });
-//   }
-
-// }
+  let contract = new web3.eth.Contract(minABI, tokenAddress);
+  let balance = await contract.methods.balanceOf(walletAddress).call();
+  balance = parseInt(balance) * dec;
+  console.log(balance);
+}
